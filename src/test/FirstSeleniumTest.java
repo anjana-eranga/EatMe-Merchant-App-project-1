05cc43b162f00fdb99d566b9ebb9e970a88ee5e8
anjana <qa@eatmeglobal.com>
2021-08-10 14:29:29 +0530

    Intial commit

---
+++ src/test/FirstSeleniumTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstSeleniumTest {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		System.setProperty("WebDriver.Chrome.driver", "C:\\chromedriver_win32");
+		driver.get("https://qa_admin.eatmeglobal.org/");
+	}
+}
